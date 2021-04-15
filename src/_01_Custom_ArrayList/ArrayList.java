2e60e73b630d1b377dbec71b77b97cb3e5022baa
kgirand <44352919+kgirand@users.noreply.github.com>
2021-04-15 16:59:57 -0700

    tsgs
    dfgsdfgsdf

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -6,27 +6,41 @@ public class ArrayList <T>{
 	
 	public ArrayList() {
 	}
-	
+	private T[] list = (T[])new Object[0];
 	public T get(int loc) throws IndexOutOfBoundsException {
-		System.out.println(loc);
-		return this.get(loc);
+		return list[loc];
 	}
 	
-	public void add(int loc) {
-		System.out.println(loc);
-		this.add(loc);
+	public void add(int loc,T val) {
+		
+		T[] newList = (T[])new Object[list.length+1];
+		for(int i = 0; i < loc; i++) {
+			newList[i] = list[i];
+		}
+		newList[newList.length-1] = val;
+		list = newList;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		this.insert(loc, val);
-	}
+		T[] newList = (T[])new Object[list.length+1];
+		for(int i = 0; i < loc; i++) {
+			newList[i+1] = list[i];
+		}
+		list = newList;
+		}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		list[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+		T[] newList = (T[])new Object[list.length-1];
+		for(int i = 0; i < loc; i++) {
+			newList[i] = list[i];
+		}
 		
+		list = newList;
+	
 	}
 	
 	public boolean contains(T val) {
